7878a445c8fb79ad901a4a8ea21eba0883afee71
Mario@kiratech.com.au <Mario@kiratech.com.au>
2021-10-26 21:55:50 +1000

    Vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,96 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <div class="row">
+      <div class="col-8">
+        <div class="row mb-3">
+          <div class="col-4">
+            <input type="text" v-model="searchText" class="form-control" />
+          </div>
+          <div class="col-2">
+            <button type="button" class="btn btn-primary" @click="searchItems">
+              Search
+            </button>
+          </div>
+        </div>
+        <div class="row justify-content-between">
+          <div
+            class="col-3"
+            v-for="(item, index) in itemList"
+            :key="item.itemId"
+            @click="selectItem(item, index)"
+          >
+            <Card
+              :itemName="item.itemName"
+              :itemImage="item.itemImage"
+              :itemDescription="item.itemDescription"
+              :itemCost="item.itemCost"
+              :itemId="item.itemId"
+            />
+          </div>
+        </div>
+      </div>
+      <div class="col border border-info">
+        <div class="row">
+          <div class="col-12" v-if="currentItem">
+            <Card
+              :itemName="currentItem.itemName"
+              :itemImage="currentItem.itemImage"
+              :itemDescription="currentItem.itemDescription"
+              :itemCost="currentItem.itemCost"
+              :itemId="currentItem.itemId"
+            />
+          </div>
+          <div class="col-12" v-else>
+            <div class="alert alert-info">Select an Item to View</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Card from "./components/Card.vue";
 
 export default {
-  name: 'App',
+  name: "App",
+  props: {},
   components: {
-    HelloWorld
-  }
-}
+    Card,
+  },
+  data() {
+    return {
+      itemList: [],
+      searchText: "",
+      currentItem: null,
+    };
+  },
+  methods: {
+    getData(searchString) {
+      fetch(`http://localhost:4825/api/ItemsWebAPI${searchString}`)
+        .then((response) => response.json())
+        .then((data) => {
+          this.itemList = data;
+          console.log(data);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    },
+    searchItems() {
+      this.getData(`?searchText=${this.searchText}`);
+    },
+    selectItem(item,index) {
+      console.log(item);
+      console.log(index);
+      this.currentItem = item;
+    }
+  },
+  
+  mounted() {
+    this.getData("");
+  },
+};
 </script>
 
 <style>
---
+++ src/components/Card.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="card m-1">
+    <div class="card-body">
+      <img class="card-img-top" :src="itemImage" />
+      <h5 class="card-tile">{{ itemName }}</h5>
+      <p class="card-text">{{ itemDescription }}</p>
+      <p class="card-text">Cost: ${{ itemCost }}</p>
+      <button type="button" class="btn btn-primary">View Details</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props: {
+    itemId: { required: true, type: Number },
+    itemName: { required: true, type: String },
+    itemDescription: { required: true, type: String },
+    itemImage: { required: true, type: String },
+    itemCost: { required: true, type: Number },
+  },
+
+  // data() {
+  //   return{
+  //     item: null,
+  //   };
+  // },
+  // methods: {
+  //   retrieveItem(itemId){
+  //     fetch(`http://localhost:4825/api/ItemsWebAPI/${itemId}`)
+  //   .then(response => response.json())
+  //   .then(data => {
+  //     this.item = data;
+  //     console.log(data);
+  //   }).catch(e => {
+  //     console.log(e);
+  //   });
+  //   }
+  // },
+
+  // mounted(){
+  //   this.retrieveItem(this.itemId);
+  // }
+};
+</script>
